304ec3b1c0e5bf936c3746aaff1341e3c5775b66
Nikolay Smirnov <nikolaysmirnov.professional@gmail.com>
2026-03-09 14:12:54 +0300

    feat: realise tests for tasks limit

---
+++ spec/integration/List.spec.tsx
@@ -0,0 +1,26 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { createTestStore } from "src/store/configureStore";
+import { Provider } from "react-redux";
+import { List } from "src/components/List";
+import { addTask, tasksSelector, uncompleteCount } from "src/store/taskSlice";
+
+it("Список содержит не больше 10 невыполненных задач", () => {
+  const store = createTestStore();
+
+  for (let i = 1; i <= 15; i++) {
+    store.dispatch(addTask(`Задача ${i}`));
+  }
+
+  render(
+    <Provider store={store}>
+      <List
+        items={tasksSelector(store.getState())}
+        onDelete={() => {}}
+        onToggle={() => {}}
+      />
+    </Provider>,
+  );
+
+  const taskItems = screen.getAllByRole("listitem");
+  expect(taskItems.length).toBe(10);
+});
